9ca30605ca3704e260e181e913a5b379f9d9c2c8
Luis Fraile <lfraile@lfraile.net>
2021-10-02 15:39:35 +0200

    Feat/vulnerability (#27)
    * Vulnerability
    
    * builds
    
    * added pwd

---
+++ src/vulnerable/VulenrableApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VulenrableApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "P~4ssw0rd";
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
