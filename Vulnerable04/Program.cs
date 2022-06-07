5b3958fad62cd81403d3cb2d587e2b645c73eb2b
Luis Fraile <lfraile@lfraile.net>
2022-06-07 17:16:33 +0200

    dependabot vulnerability [skip ci]

---
+++ Vulnerable04/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Vulnerable03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
