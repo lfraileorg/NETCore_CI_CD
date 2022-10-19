0c67e3569af2d8b432f8e52fc133d2efc1dc0c43
Luis Fraile <lfraile@lfraile.net>
2022-10-19 18:26:24 +0000

    Pending changes exported from your codespace

---
+++ .devcontainer/init-db.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# set ASPNETCORE_ENVIRONMENT=Codespaces
+
+sudo dotnet dev-certs https
+dotnet tool restore 
+
+dotnet build ./src/Web/Web.csproj
+
+# dotnet ef database update -c catalogcontext -p ./src/Infrastructure/Infrastructure.csproj -s ./src/Web/Web.csproj
+# dotnet ef database update -c appidentitydbcontext -p ./src/Infrastructure/Infrastructure.csproj -s ./src/Web/Web.csproj
