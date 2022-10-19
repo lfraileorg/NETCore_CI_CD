8a9231a4fbe148ba504b25edd189415ac7ebfe6b
Luis Fraile <lfraile@lfraile.net>
2022-10-19 19:32:41 +0000

    EF init

--- .devcontainer/init-db.sh
+++ .devcontainer/init-db.sh
@@ -6,5 +6,5 @@ dotnet tool restore
 
 dotnet build ./src/Web/Web.csproj
 
-# dotnet ef database update -c catalogcontext -p ./src/Infrastructure/Infrastructure.csproj -s ./src/Web/Web.csproj
-# dotnet ef database update -c appidentitydbcontext -p ./src/Infrastructure/Infrastructure.csproj -s ./src/Web/Web.csproj
+dotnet ef database update -c catalogcontext -p ./src/Infrastructure/Infrastructure.csproj -s ./src/Web/Web.csproj
+dotnet ef database update -c appidentitydbcontext -p ./src/Infrastructure/Infrastructure.csproj -s ./src/Web/Web.csproj
